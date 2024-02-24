7a6e392bbfceae5861b2cfe6c40cadb2d3260715
Mihai Nicolae <m.v.nicolae@student.tudelft.nl>
2024-02-24 10:15:23 +0100

    New methods to create and join events

--- client/src/main/java/client/scenes/StarterPageCtrl.java
+++ client/src/main/java/client/scenes/StarterPageCtrl.java
@@ -1,11 +1,14 @@
 package client.scenes;
 
 import client.utils.ServerUtils;
+import commons.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class StarterPageCtrl {
@@ -17,22 +20,54 @@ public class StarterPageCtrl {
     @FXML
     private TextField joinEvent;
 
+    private String eventName;
+    private List<Event> eventList;
+
     public StarterPageCtrl(ServerUtils server) {
         this.server = server;
+        this.eventList = new ArrayList<>();
     }
 
-    @Inject
-    public void CreateNewEvent(ServerUtils server) {
-        // to be implemented
+    public StarterPageCtrl(ServerUtils server, List<Event> list) {
+        this.server = server;
+        this.eventList = list;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public List<Event> getEventList() {
+        return eventList;
+    }
+
+    public void setEventList(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+
+    public void createNewEvent(ServerUtils server) {
+        eventName = createNewEvent.getText();
+        Event newEvent = new Event(eventName);
+        eventList.add(newEvent);
     }
 
     public void joinEvent(ServerUtils server) {
-        // to be implemented
+        eventName = createNewEvent.getText();
+        Event newEvent = new Event(eventName);
+        eventList.add(newEvent);
     }
 
     public void keyPressed(KeyEvent e) {
         switch(e.getCode()) {
             case ENTER:
+                if(createNewEvent.getText() == null || createNewEvent.getText().equals(""))
+                    joinEvent(server);
+                else
+                    createNewEvent(server);
                 break;
             default:
                 break;
