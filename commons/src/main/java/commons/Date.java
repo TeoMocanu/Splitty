92754f0b98932c65e2d8eca2101d024f88c0f630
Mihai Nicolae <m.v.nicolae@student.tudelft.nl>
2024-02-25 19:06:56 +0100

    Created Date Class

---
+++ commons/src/main/java/commons/Date.java
@@ -0,0 +1,65 @@
+package commons;
+
+import java.util.Objects;
+
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int day, int month, int year) throws Exception {
+        if(day > 31 || day > 30 && month % 2 == 0 || day < 0)
+            throw new Exception("Invalid date");
+        if(month < 0 || month > 12)
+            throw new Exception("Invalid date");
+        if(year < 0)
+            throw new Exception("Invalid date");
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Date date)) return false;
+        return day == date.day && month == date.month && year == date.year;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(day, month, year);
+    }
+
+    public String toStringMDY() {
+        return day + "." + month + "." + year;
+    }
+
+    public String toStringDMY() {
+        return month + "." + day + "." + year;
+    }
+}
