ffc4a6dba1515d7ac15e9c23292c41246ca9c9bd
Matei Pavel Dumitrescu <M.P.Dumitrescu@student.tudelft.nl>
2024-03-15 22:53:57 +0100

    Features for AdminLogin

--- client/src/main/java/client/scenes/AdminLoginCtrl.java
+++ client/src/main/java/client/scenes/AdminLoginCtrl.java
@@ -3,8 +3,10 @@ package client.scenes;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import jakarta.ws.rs.WebApplicationException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Modality;
@@ -16,6 +18,17 @@ public class AdminLoginCtrl {
     private String adminPassword;
 
     @FXML
+    private Button backButton;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private Button languageButton;
+
+    private String currentLanguage;
+
+    @FXML
     private TextField password;
     @Inject
     public AdminLoginCtrl(ServerUtils server, MainCtrl mainCtrl) {
@@ -24,15 +37,37 @@ public class AdminLoginCtrl {
     }
     @FXML
     private void initialize(){
+        currentLanguage = "EN";
+        languageButton.setText("EN");
         ServerUtils utils = new ServerUtils();
         String randomPassword = utils.generateRandomPassword(16); // Generates a 16-byte password, encoded in Base64
         this.adminPassword=randomPassword;
         System.out.println("Admin Password: " + randomPassword);
     }
+    public void language(){
+        if(currentLanguage.equals("EN")){
+            currentLanguage = "NL";
+            nl();
+        }
+        else{
+            currentLanguage = "EN";
+            en();
+        }
+    }
+    public void en(){
+        languageButton.setText("EN");
+        enterButton.setText("ENTER");
+        backButton.setText("BACK");
+    }
+    public void nl(){
+        languageButton.setText("NL");
+        enterButton.setText("INVOEREN");
+        backButton.setText("TERUG");
+    }
     public void cancel() {
-        System.out.println("Existed admin login");
+        System.out.println("Exited admin login");
         clearFields();
-        mainCtrl.showOverview();
+        mainCtrl.showStarterPage();
     }
     public void checkPassword() {
         try {
@@ -75,5 +110,11 @@ public class AdminLoginCtrl {
             default:
                 break;
         }
+    }
+
+    public void backToStart(ActionEvent actionEvent) {
+        System.out.println("Exited admin login");
+        clearFields();
+        mainCtrl.showStarterPage();
     }
 }
