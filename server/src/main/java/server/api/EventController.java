365f4ecf9a9014bcf02cfac830811eaa08fe8390
Matej Kliment <M.Kliment@student.tudelft.nl>
2024-03-24 19:23:22 +0100

    fixed checkstyle

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -1,7 +1,6 @@
 package server.api;
 
 import commons.Event;
-import commons.Expense;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- server/src/main/java/server/api/ExpenseController.java
+++ server/src/main/java/server/api/ExpenseController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.ExpenseRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
