b8ab134727f51c944569e5fa8d34feeca3e152b1
Sebastian Mustaţă <D.Mustata@student.tudelft.nl>
2024-04-14 23:31:02 +0200

    Checkstyle

--- server/src/main/java/server/services/EmailerService.java
+++ server/src/main/java/server/services/EmailerService.java
@@ -1,7 +1,6 @@
 package server.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
